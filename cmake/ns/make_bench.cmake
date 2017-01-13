5beba6289431e0e331fadb86465516cfe7db1ca5
Charly Chevalier <charly.chevalier@numscale.com>
2017-01-13 17:05:14 +0100

    Tries to minimize cmake generation time

--- cmake/ns/make_bench.cmake
+++ cmake/ns/make_bench.cmake
@@ -11,26 +11,43 @@ NS_guard(NS_CMAKE_MAKE_BENCH)
 NS_include(compilers)
 NS_include(add_target_parent)
 
-## Process a list of source files to generate corresponding bench target
+## INTERNAL: Setup bench target
+## -------------------------------------------------------------------------------------------------
+function(_make_bench bench file)
+  add_executable(${bench} ${file})
+  set_property( TARGET ${bench}
+                PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bench"
+              )
+  set_target_properties ( ${bench} PROPERTIES
+                          EXCLUDE_FROM_DEFAULT_BUILD TRUE
+                          EXCLUDE_FROM_ALL TRUE
+                        )
+  if (MAKE_BENCH_TARGET_LINK_LIBRARIES)
+      target_link_libraries(${bench} ${MAKE_BENCH_TARGET_LINK_LIBRARIES})
+  endif()
+
+  message(STATUS ${bench})
+  add_target_parent(${bench})
+  add_dependencies(bench ${bench})
+endfunction()
+
+## Process a list of source files to generate corresponding bench target and prepend a root name
 ## -------------------------------------------------------------------------------------------------
 function(make_bench root)
   foreach(file ${ARGN})
     string(REPLACE ".cpp" ".bench" base ${file})
     set(bench "${root}.${base}")
-
-    add_executable(${bench} ${file})
-    set_property( TARGET ${bench}
-                  PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bench"
-                )
-    set_target_properties ( ${bench} PROPERTIES
-                            EXCLUDE_FROM_DEFAULT_BUILD TRUE
-                            EXCLUDE_FROM_ALL TRUE
-                          )
-    if (MAKE_BENCH_TARGET_LINK_LIBRARIES)
-        target_link_libraries(${bench} ${MAKE_BENCH_TARGET_LINK_LIBRARIES})
-    endif()
-
-    add_target_parent(${bench})
-    add_dependencies(bench ${bench})
+    _make_bench(${bench} ${file})
   endforeach()
 endfunction()
+
+## Process a list of source files to generate corresponding bench target
+## -------------------------------------------------------------------------------------------------
+function(make_bench_from_sources)
+  foreach(file ${ARGN})
+    string(REPLACE ".cpp" ".bench" base ${file})
+    string(REPLACE "/" "." bench ${base})
+    _make_bench(${bench} ${file})
+  endforeach()
+endfunction()
+
